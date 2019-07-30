5d659f862a03eacac60fb001ca8293df9643e687
Rebecca Pae <r.pae92@gmail.com>
2019-07-29 22:35:44 -0400

    Updates

---
+++ precache-manifest.b5843ce4b5811151a2111e844935362d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48fbcd41985978558d330acd8d7568d",
+    "url": "/clicky.game.github.io/index.html"
+  },
+  {
+    "revision": "21f40cb5a40737e55325",
+    "url": "/clicky.game.github.io/static/css/main.dac8f5cc.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky.game.github.io/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "21f40cb5a40737e55325",
+    "url": "/clicky.game.github.io/static/js/main.8f346bf8.chunk.js"
+  },
+  {
+    "revision": "bab75d79fe4f416ad94e",
+    "url": "/clicky.game.github.io/static/js/runtime~main.23abf5e0.js"
+  }
+]);
