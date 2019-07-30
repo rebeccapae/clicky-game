57628a0464ef3187534c1f2944b795867f677f2d
Rebecca Pae <r.pae92@gmail.com>
2019-07-29 23:19:05 -0400

    Updates

---
+++ precache-manifest.8e4d35f78b67ef844d5ffe0dfef6f769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b6327a95c7008e644cee66d24a9cb4",
+    "url": "/clicky.game.github.io/index.html"
+  },
+  {
+    "revision": "770e742ee12732fcd77b",
+    "url": "/clicky.game.github.io/static/css/main.dac8f5cc.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky.game.github.io/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "770e742ee12732fcd77b",
+    "url": "/clicky.game.github.io/static/js/main.cedc2b8f.chunk.js"
+  },
+  {
+    "revision": "bab75d79fe4f416ad94e",
+    "url": "/clicky.game.github.io/static/js/runtime~main.23abf5e0.js"
+  }
+]);
--- src/components/Points/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Points(props) {
-  return <h2 className="points">{props.children}</h2>;
-}
-
-export default Points;
